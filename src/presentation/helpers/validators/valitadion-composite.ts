cf152a3060a66c756026c0275fce4390f83dcfba
Matheus Sabino <matheusdsb@gmail.com>
2021-02-27 20:01:22 -0300

    refactor: move requried field validation to the ValidationComposite

--- src/presentation/helpers/validators/valitadion-composite.ts
+++ src/presentation/helpers/validators/valitadion-composite.ts
@@ -1,9 +1,7 @@
 import { Validation } from './validation'
 
 export class ValidationComposite implements Validation {
-  constructor (private readonly validations: Validation[]) {
-
-  }
+  constructor (private readonly validations: Validation[]) { }
 
   validate (input: any): Error | null {
     for (const validation of this.validations) {
