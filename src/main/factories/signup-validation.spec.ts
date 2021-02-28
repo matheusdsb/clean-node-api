38c9bbf456c3ce502f55795be5eb4ce9fd719a77
Matheus Sabino <matheusdsb@gmail.com>
2021-02-28 19:10:23 -0300

    refactor: move passord comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { ValidationComposite } from '../../presentation/helpers/validators/valit
 import { makeSignUpValidation } from './signup-validation'
 import { RequiredFieldVaildation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
+import { CompareFieldsVaildation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/valitadion-composite')
 
@@ -12,6 +13,7 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldVaildation(field))
     }
+    validations.push(new CompareFieldsVaildation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,11 +1,13 @@
 import { ValidationComposite } from '../../presentation/helpers/validators/valitadion-composite'
 import { RequiredFieldVaildation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
+import { CompareFieldsVaildation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldVaildation(field))
   }
+  validations.push(new CompareFieldsVaildation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
