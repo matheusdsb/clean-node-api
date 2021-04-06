5d34f5cb6d9281c75c69b03c26af9a2eb4135345
Matheus Sabino <matheusdsb@gmail.com>
2021-04-06 00:45:56 -0300

    feat: ensure DbAddSUrvey calss AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey-protocols.ts
@@ -0,0 +1,4 @@
+
+export * from '../../../domain/usecases/add-survey'
+export * from '../../protocols/db/account/add-account-repository'
+export * from '../../protocols/db/survey/add-survey-repository'
