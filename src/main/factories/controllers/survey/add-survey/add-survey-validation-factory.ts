fa6b3d05ce909d397373d6b1f8de1c48d11c0316
Matheus Sabino <matheusdsb@gmail.com>
2021-04-10 19:39:21 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
@@ -1,5 +1,5 @@
-import { ValidationComposite, RequiredFieldVaildation } from '../../../../validation/validators'
-import { Validation } from '../../../../presentation/protocols/validation'
+import { ValidationComposite, RequiredFieldVaildation } from '../../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols/validation'
 
 export const makeAddSurveyValidation = (): ValidationComposite => {
   const validations: Validation[] = []
