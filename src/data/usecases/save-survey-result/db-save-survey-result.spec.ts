ace8adfa9b43de2975a24c1fb9ab49d7e138f70f
Matheus Sabino <matheusdsb@gmail.com>
2021-04-29 00:33:26 -0300

    test: ensure DbSaveSurrveyResul throws if DbSaveSurveyResultRepository throws

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -52,4 +52,11 @@ describe('DbSaveSurveyResult UseCase', () => {
     await sut.save(makeFakeSurveyResultData())
     expect(saveSpy).toHaveBeenCalledWith(makeFakeSurveyResultData())
   })
+
+  test('Should throw if SaveSurveyResultRepository throws', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(saveSurveyResultRepositoryStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.save(makeFakeSurveyResultData())
+    await expect(promise).rejects.toThrow(new Error())
+  })
 })
